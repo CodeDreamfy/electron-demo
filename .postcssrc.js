61acdf309ce44a2f82ec04522cdf7b6a7f767ea8
codedreamfy@outlook.com <codedreamfy@outlook.com>
2018-07-25 17:19:48 +0800

    init

---
+++ .postcssrc.js
@@ -0,0 +1,16 @@
+// https://github.com/michael-ciniawsky/postcss-load-config
+
+module.exports = {
+  // syntax: 'postcss-less',
+  "plugins": {
+    "precss": {},
+    "postcss-import": {},
+    "postcss-url": {},
+    // "postcss-px2rem": {
+    //   remUnit: 12
+    // },
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {},
+    "cssnano": {}
+  }
+}
---
+++ src/components/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="index-wrap">
+    <div class="inner-wrap">
+      <el-row>
+        <el-col :span="6">
+          <div class="grid-content bg-purple list-devices">
+
+          </div>
+        </el-col>
+        <el-col :span="18"><div class="grid-content bg-purple-light device-main"></div></el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+@import url(../css/index.css);
+</style>
+
